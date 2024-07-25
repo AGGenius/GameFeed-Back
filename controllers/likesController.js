d9ce02ed3eaa3a71b838a34c816ccf1ce6eeb1da
Adrian <adrianginergimenez@gmail.com>
2024-07-25 14:00:58 +0200

    feat: improved like management

--- controllers/likesController.js
+++ controllers/likesController.js
@@ -1,5 +1,14 @@
 const client = require('../db.js');
 
+const getLikes = async (req, res) => {
+    try {
+        const result = await client.query(`SELECT * FROM likes `);
+        res.json(result.rows);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
 const createLike = async (gameId, postId) => { 
     try {
         await client.query(`INSERT INTO likes (game_id, post_id, value) VALUES ($1, $2, $3)`, [ gameId, postId, 0]);
@@ -9,7 +18,7 @@ const createLike = async (gameId, postId) => {
     }
 }
 
-const createUserLike = async (req, res) => { 
+const controlUserLike = async (req, res) => { 
     try {
         const { user_id, likes_id} = req.body;
 
@@ -17,28 +26,22 @@ const createUserLike = async (req, res) => {
 
         if (likeCheck.rows.length === 0) {
             await client.query(`INSERT INTO userslikes (active, user_id, likes_id) VALUES ($1, $2, $3)`, [true, user_id, likes_id]);
-            res.json({ estado: "Like incrementado"});
         } else {
             await client.query('UPDATE userslikes SET active = $3 WHERE user_id = $1 AND likes_id = $2', [user_id, likes_id, !likeCheck.rows[0].active]); //Updatear en un endpoint post?
-            res.json({ estado: `Like cambiado ${likeCheck.rows[0].active}`});
         }
 
-        setLikesById(likes_id);
+        try {
+            const actualLikes = await client.query('SELECT * FROM usersLikes WHERE likes_id = $1 AND active = true', [likes_id])
+            await client.query('UPDATE likes SET value = $1 WHERE id = $2', [actualLikes.rows.length, likes_id]); 
+        } catch (error) {
+            console.log(error);
+        }
 
+        getLikes(req,res);
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 }
 
 
-const setLikesById = async (likes_id) => {
-    try {
-        const actualLikes = await client.query('SELECT * FROM usersLikes WHERE likes_id = $1 AND active = true', [likes_id])
-        await client.query('UPDATE likes SET value = $2 WHERE id = $1', [likes_id, actualLikes.rows.length]); //Updatear en un endpoint post?
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-
-module.exports = { createLike, createUserLike}
+module.exports = {getLikes, createLike, controlUserLike}
--- routes/likesRoutes.js
+++ routes/likesRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const likesController = require('../controllers/likesController');
 
-
-router.post('/', likesController.createUserLike);
+router.get('/', likesController.getLikes)
+router.post('/', likesController.controlUserLike);
 
 module.exports = router;
