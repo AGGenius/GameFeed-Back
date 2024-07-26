b31db78266516519696cc27a17d7569ce8207b84
Adrian <adrianginergimenez@gmail.com>
2024-07-26 14:18:14 +0200

    fix: improvement on game endpoints

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -32,8 +32,15 @@ const getGames = async (req, res) => {
 const getGameById = async (req, res) => { 
     try {
         const { id } = req.params;
+
         const result = await client.query('SELECT * FROM games WHERE id= $1', [id]);
-        res.json(result.rows);
+        
+        if(result.rows.length > 0) {
+            res.json(result.rows);
+        } else {
+            res.json({ estado: "Juego no encontrado"})
+        }
+        
     } catch (error) {
         res.status(500).json({ error: error.message});
     }
@@ -43,8 +50,8 @@ const editGameById = async (req, res) => {
     try {
         const { id } = req.params;
 
-        const { tittle, genre, developer, release, user_id } = req.body;
-        await client.query('UPDATE games SET tittle = $2, genre = $3, developer = $4, release = $5, user_id = $6 WHERE id = $1', [id, tittle, genre, developer, release, user_id]);
+        const { tittle, genre, developer, release, user_id, active } = req.body;
+        await client.query('UPDATE games SET tittle = $2, genre = $3, developer = $4, release = $5, user_id = $6, active = $7 WHERE id = $1', [id, tittle, genre, developer, release, user_id, active]);
         res.json({ estado: "Juego actualizado correctamente"});
     } catch (error) {
         res.status(500).json({ error: error.message});
