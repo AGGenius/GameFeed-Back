3ab548d30c25d5eb88d05955130283729c998c24
Adrian <adrianginergimenez@gmail.com>
2024-12-16 12:57:26 +0100

    fix: change of DDBB host

--- src/db.js
+++ src/db.js
@@ -1,21 +1,14 @@
-const { Client} = require('pg');
 require('dotenv').config();
 
-const client = new Client({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    ssl: true
-});
+const { neon } = require('@neondatabase/serverless');
 
-client.connect(error => {
-    if (error) {
-        console.log('Connection error', error.stack);
-    } else {
-        console.log('Connected to database');
-    };
-});
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
 
-module.exports = client;
+const sql = neon(`postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?sslmode=require`);
+
+async function getPgVersion() {
+  const result = await sql`SELECT version()`;
+  console.log(result[0]);
+}
+
+module.exports = getPgVersion;
